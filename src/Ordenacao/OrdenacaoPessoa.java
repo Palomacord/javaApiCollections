501b054555eabbf5bd83571738b9286e1f8ccf65
paloma <palomacorrdeiro@hotmail.com>
2023-10-18 10:58:18 -0300

    Ordenacao de listas

---
+++ src/Ordenacao/OrdenacaoPessoa.java
@@ -0,0 +1,44 @@
+package Ordenacao;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class OrdenacaoPessoa {
+
+    private  List<Pessoa> pessoaList;
+    public static void main(String[] args) {
+        OrdenacaoPessoa ordenacaoPessoa = new OrdenacaoPessoa();
+        ordenacaoPessoa.adicionarPessoa("Paloma",26,1.62);
+        ordenacaoPessoa.adicionarPessoa("Emanuela",30,1.70);
+        ordenacaoPessoa.adicionarPessoa("Ruan",29,1.82);
+        System.out.println(ordenacaoPessoa.ordenarPorIdade());
+        System.out.println(ordenacaoPessoa.ordenarPorAltura());
+       // System.out.println( ordenacaoPessoa.obtemTamanhoPessoas());
+       ;
+    }
+    public OrdenacaoPessoa(){
+        this.pessoaList = new ArrayList<>();
+    }
+
+    public void adicionarPessoa(String nome, int idade, double altura){
+        pessoaList.add(new Pessoa(nome, idade, altura));
+    }
+
+    public List<Pessoa> ordenarPorIdade(){
+        List<Pessoa> pessoasPorIdade = new ArrayList<>(pessoaList);
+        Collections.sort(pessoasPorIdade);
+
+        return  pessoasPorIdade;
+    }
+
+    public  List<Pessoa> ordenarPorAltura(){
+        List<Pessoa> pessoasPorAltura = new ArrayList<>(pessoaList);
+        Collections.sort(pessoasPorAltura,new ComparatorPorAltura());
+        return pessoasPorAltura;
+    }
+
+    public  int obtemTamanhoPessoas() {
+        return pessoaList.size();
+    }
+}
